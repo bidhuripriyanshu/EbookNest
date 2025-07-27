80e025ad6d74b765e1620e3fd1fbac4a0ce3fb83
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2025-07-27 21:44:23 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -9,13 +9,17 @@ import { postLogin, postSignup } from './controllers/User.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ["https://ebook-platform-online-icy6.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,  
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://ebook-platform-online-icy6.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
     }
-));
+    next();
+});
 
 
 try {
